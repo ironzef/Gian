0bc69ead33f783e80e8cb927de27782396b99628
Adhish <83854239+ironzef@users.noreply.github.com>
2022-07-28 23:14:18 +0530

    Add files via upload

---
+++ static/scripts/responses.js
@@ -0,0 +1,311 @@
+function getBotResponse(input) {
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there🙌!";
+    } else if (input == "goodbye") {
+        return "Talk to you later😮‍💨!"; 
+    }
+   else if (input == "bye") {
+    return "Talk to you later😮‍💨!"; 
+} else if (input == "Bye") {
+    return "Talk to you later😮‍💨!"; 
+} else if (input == "Who are you?") {
+    return "I Am Gian The Bot💀"; 
+}
+else if (input == "who are you?") {
+    return "I am your truly Gian the bot(❁´◡`❁)"; 
+} else if (input == "who are you") {
+    return "I am your truly Gian the bot😎✌️"; 
+} else if (input == "Who are you") {
+    return "I am Your Most Beautiful and smart Bot Gian😎"; 
+} 
+else if (input == "download python") {
+    window.open("https://www.python.org/","_bank")
+    return ["Step 1 − Select Version of Python to Install. ..." +
+    "Step 2 − Download Python Executable Installer. ..."+
+    "Step 3 − Run Executable Installer And Check All Boxes. ..."+
+    "Step 4 − Verify Python is installed on Windows. ..."+
+    "Step 5 − Verify Pip was installed."]; 
+}
+else if (input == "Download python") {
+    window.open("https://www.python.org/","_bank")
+    return ["Step 1 − Select Version of Python to Install. ..." +
+    "Step 2 − Download Python Executable Installer. ..."+
+    "Step 3 − Run Executable Installer And Check All Boxes. ..."+
+    "Step 4 − Verify Python is installed on Windows. ..."+
+    "Step 5 − Verify Pip was installed."];
+}
+else if (input == "Install python") {
+    window.open("https://www.python.org/","_bank")
+    return ["Step 1 − Select Version of Python to Install. ..." +
+    "Step 2 − Download Python Executable Installer. ..."+
+    "Step 3 − Run Executable Installer And Check All Boxes. ..."+
+    "Step 4 − Verify Python is installed on Windows. ..."+
+    "Step 5 − Verify Pip was installed."];
+}
+else if (input == "install python") {
+    window.open("https://www.python.org/","_bank")
+    return ["Step 1 − Select Version of Python to Install. ..." +
+    "Step 2 − Download Python Executable Installer. ..."+
+    "Step 3 − Run Executable Installer And Check All Boxes. ..."+
+    "Step 4 − Verify Python is installed on Windows. ..."+
+    "Step 5 − Verify Pip was installed."];
+}
+else if (input == "How to download python?") {
+    window.open("https://www.python.org/","_bank")
+    return ["Step 1 − Select Version of Python to Install. ..." +
+    "Step 2 − Download Python Executable Installer. ..."+
+    "Step 3 − Run Executable Installer And Check All Boxes. ..."+
+    "Step 4 − Verify Python is installed on Windows. ..."+
+    "Step 5 − Verify Pip was installed."];
+}
+else if (input == "how to download python?") {
+    window.open("https://www.python.org/","_bank")
+    return ["Step 1 − Select Version of Python to Install. ..." +
+    "Step 2 − Download Python Executable Installer. ..."+
+    "Step 3 − Run Executable Installer And Check All Boxes. ..."+
+    "Step 4 − Verify Python is installed on Windows. ..."+
+    "Step 5 − Verify Pip was installed."];
+}
+else if (input == "how to download python") {
+    window.open("https://www.python.org/","_bank")
+    return ["Step 1 − Select Version of Python to Install. ..." +
+    "Step 2 − Download Python Executable Installer. ..."+
+    "Step 3 − Run Executable Installer And Check All Boxes. ..."+
+    "Step 4 − Verify Python is installed on Windows. ..."+
+    "Step 5 − Verify Pip was installed."];
+}
+else if (input == "How to download python") {
+    window.open("https://www.python.org/","_bank")
+    return ["Step 1 − Select Version of Python to Install. ..." +
+    "Step 2 − Download Python Executable Installer. ..."+
+    "Step 3 − Run Executable Installer And Check All Boxes. ..."+
+    "Step 4 − Verify Python is installed on Windows. ..."+
+    "Step 5 − Verify Pip was installed."];
+}
+else if (input == "Step to download python") {
+    window.open("https://www.python.org/","_bank")
+    return ["Step 1 − Select Version of Python to Install. ..." +
+    "Step 2 − Download Python Executable Installer. ..."+
+    "Step 3 − Run Executable Installer And Check All Boxes. ..."+
+    "Step 4 − Verify Python is installed on Windows. ..."+
+    "Step 5 − Verify Pip was installed."];
+}
+else if (input == "step to download python") {
+    window.open("https://www.python.org/","_bank")
+    return ["Step 1 − Select Version of Python to Install. ..." +
+    "Step 2 − Download Python Executable Installer. ..."+
+    "Step 3 − Run Executable Installer And Check All Boxes. ..."+
+    "Step 4 − Verify Python is installed on Windows. ..."+
+    "Step 5 − Verify Pip was installed."];
+}
+else if (input == "play song") {
+    window.open("https://youtube.com/shorts/BOoQRYaEp34?feature=share","_bank")
+    return "Okay here is my personal favrate song❤️🙌 " 
+}
+else if (input == "Play song") {
+    window.open("https://youtube.com/shorts/BOoQRYaEp34?feature=share","_bank")
+    return "Okay here is my personal favrate song❤️🙌 " 
+}
+else if (input == "play sad song") {
+    window.open("https://www.youtube.com/shorts/yBWrQbimzSU","_bank")
+    return "Okay here is my personal favrate song❤️🙌 "
+   
+}
+else if (input == "Play sad song") {
+    window.open("https://www.youtube.com/shorts/yBWrQbimzSU","_bank")
+    return "Okay here is my personal favrate song❤️🙌 "
+   
+}
+else if (input == "Play party song") {
+    window.open("https://www.youtube.com/watch?v=vMwjT26baE0","_bank")
+    return "Okay here is my personal favrate song❤️🙌 "
+   
+}
+else if (input == "play party song") {
+    window.open("https://www.youtube.com/watch?v=vMwjT26baE0","_bank")
+    return "Okay here is my personal favrate song❤️🙌 "
+}
+else if (input == "install VScode") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "Install VScode") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "install VS code") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "Install VS code") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "install vs code") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "Install vscode") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "install vscode") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "Download VScode") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "download VScode") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "Download vscode") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "download vscode") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "How to install VScode?") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "How to download VScode") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "how to install VScode?") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "how to install VScode?") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "how to download VScode") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "How to download vscode?") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "How to download vscode") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "how to download vscode?") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "how to download vscode") {
+    window.open("https://code.visualstudio.com/","_bank")
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "steps to install VScode?") {
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "Steps to install VScode?") {
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "steps to install vscode?") {
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "Steps to installl vscode?") {
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "Steps to install VScode") {
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "steps to install VScode") {
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "Steps to download vscode?") {
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "steps to download vscode") {
+    return ["Step 1 :Download VS code from here Link.Step 2 :Download the Visual Studio Code installer for Windows. Once it is downloaded, run the installer (VSCodeUserSetup-{version}.exe). Then, run the file – it will only take a minute."];
+}
+else if (input == "Who made you?") {
+    return "Adhish Gupta From class 10th B Made me for no reason...Type 'yes' to know more about him"
+}
+else if (input == "who made you?") {
+    return "Adhish Gupta From class 10th B Made me for no reason...Type 'yes' to know more about him"
+}
+else if (input == "Who made you") {
+    return "Adhish Gupta From class 10th B Made me for no reason...Type 'yes' to know more about him"
+}
+else if (input == "who made you") {
+    return "Adhish Gupta From class 10th B Made me for no reason...Type 'yes' to know more about him"
+}
+else if (input == "yes") {
+    window.open("https://finfolio.ml/","_bank")
+    return "There you goo!" 
+}
+else if (input == "Yes") {
+    window.open("https://finfolio.ml/","_bank") 
+    return "There you goo!"
+}
+else if (input == "About you") {
+   return "I am Gian your truly handsome bot. I can do little things and i was made by a Truly handsome boi Adhish Gupta He is studying in class 10b type 'yes' to know more about him."
+}
+else if (input == "about you") {
+    return "I am Gian your truly handsome bot. I can do little things and i was made by a Truly handsome boi Adhish Gupta He is studying in class 10b type 'yes' to know more about him."
+ }
+ else if (input == "hi") {
+    return"Hello there🙌"
+ }
+ else if (input == "How are you?") {
+    return"Doing Good✌️"
+ }
+ else if (input == "how are you?") {
+    return"Doing Good✌️"
+ }
+ else if (input == "how are you") {
+    return"Doing Good✌️"
+ }
+ else if (input == "How are you") {
+    return"Doing Good✌️"
+ }
+ else if (input == "Good morning") {
+    return"Good Morning"
+ }
+ else if (input == "good morning") {
+    return"Good Morning"
+ }
+ else if (input == "Good evening") {
+    return"Good evening"
+ }
+ else if (input == "good evening") {
+    return"Good evening"
+ }
+ else if (input == "play killer song") {
+    window.open("https://youtu.be/XVg6XjbcwoA","_bank")
+    window.open("https://youtu.be/4izNgBvab2o","_bank")
+    return"okay!"
+ }
+ else if (input == "Play killer song") {
+    window.open("https://youtu.be/XVg6XjbcwoA","_bank")
+    window.open("https://youtu.be/4izNgBvab2o","_bank")
+    return"okay!"
+ }
+ else if (input == "jokes") {
+    return" What does a cloud wear under his raincoat?.................Thunderwear."
+ }
+ else if (input == "Jokes") {
+    return" What does a cloud wear under his raincoat?.................Thunderwear."
+ }
+ else if (input == "help") {
+    return"Commands: Good morning/evening/night, About You, Who made you, Install python, install VScode, steps to install python, Steps to install VScode, play song, play party song, play sad song, play killer song, your codes, jokes And many more basic stuff.  "
+ }  
+ else if (input == "Help") {
+    return"Commands: Good morning/evening/night, About You, Who made you, Install python, install VScode, steps to install python, Steps to install VScode, play song, play party song, play sad song, play killer song, your codes, jokes And many more basic stuff.  "
+ }  
+    else {
+        return "Try asking something else I am Still Thinking About it! OR type Help";
+    }
+}
